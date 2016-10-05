2d4ba3f09063c64207106252ebcb822eddbe3642
Lynn Cyrin <firemagelynn@gmail.com>
2016-10-04 19:37:56 -0700

    disable the very time consuming IWC test

--- test/test_IWC_integration.py
+++ test/test_IWC_integration.py
@@ -20,13 +20,13 @@ from lib import utils, IWC_integration
 ############################################################
 
 
-def test_image_from_video_url():
-    table = utils.get_table()
+# def test_image_from_video_url():
+#     table = utils.get_table()
 
-    for name, video in table.items():
-        stripped_name = name.replace(',','').replace(' ','')
-        vcr_path = 'test/vcr/test_IWC_integration/video{}.yaml'.format(stripped_name)
+#     for name, video in table.items():
+#         stripped_name = name.replace(',','').replace(' ','')
+#         vcr_path = 'test/vcr/test_IWC_integration/video{}.yaml'.format(stripped_name)
 
-        with vcr.use_cassette('test/vcr/test_IWC_integration/video_{}.yaml'.format(stripped_name)):
-            image = IWC_integration.image_from_video_url(video['link'])
-            assert image[-4:] == '.gif'
+#         with vcr.use_cassette('test/vcr/test_IWC_integration/video_{}.yaml'.format(stripped_name)):
+#             image = IWC_integration.image_from_video_url(video['link'])
+#             assert image[-4:] == '.gif'
