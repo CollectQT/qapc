99fa0779b01350ef410ffdc17017185c845ddd50
Lynn Cyrin <firemagelynn@gmail.com>
2016-10-04 23:16:32 -0700

    reorganize frontend routes, add contact route

--- main.py
+++ main.py
@@ -6,42 +6,43 @@ import flask
 from lib import utils, view_handlers
 
 
-# start app and set configuration
+############################################################
+# setup
+############################################################
+
+
 app = flask.Flask(__name__, static_folder='static', static_url_path='/static', )
 cache = utils.setup(app)
 
-# Views! i.e. what the user gets when they type in our url
+
+############################################################
+# views
+############################################################
 
 
-@app.route('/sales')
-# @cache.cached(timeout=84600, unless=app.config['DEBUG'])
-def sales():
-    return flask.render_template('sales.html', table=view_handlers.get_and_populate_shoot_table())
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/contact')
+def contact():
+    return flask.render_template('contact.html')
 
 
 @app.route('/profile/<worker_key>')
-# @cache.cached(timeout=84600, unless=app.config['DEBUG'])
 def profile(worker_key):
     return flask.render_template('profile.html', worker=view_handlers.get_user_profile_info(worker_key))
 
 
-@app.route('/')
-def index ():
-    return flask.render_template('index.html')
+# @app.route('/sales')
+# def sales():
+#     return flask.render_template('sales.html', table=view_handlers.get_and_populate_shoot_table())
 
 
-@app.route('/example')
-def example_route():
-    return flask.render_template('partials/base.html',
-        content='''
-# Example Page
-
-You can create an entire new page be adding content inline here!
-
-You probably shouldn't, but you can.
-
-Syntax is markdown, and the lack of column alignment is on purpose
-    ''')
+############################################################
+# error pages
+############################################################
 
 
 @app.errorhandler(404)
@@ -54,6 +55,11 @@ def page_not_found(e):
 def server_error(e):
     return flask.render_template('partials/base.html',
         content='# Error 500\nServer error'), 500
+
+
+############################################################
+# dev mode startup
+############################################################
 
 
 if __name__ == '__main__':
