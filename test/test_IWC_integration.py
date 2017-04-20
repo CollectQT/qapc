7b9fade6c7587ee86233fe197bfb87c2b6586316
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-20 11:05:30 -0700

    fix IWC test

--- test/test_IWC_integration.py
+++ test/test_IWC_integration.py
@@ -12,7 +12,7 @@ import vcr
 
 base_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 sys.path.append(base_dir)
-from lib import utils, IWC_integration
+from lib import IWC_integration, view_handlers
 
 
 ############################################################
@@ -20,16 +20,13 @@ from lib import utils, IWC_integration
 ############################################################
 
 
-def test_true(): assert True
+def test_image_from_video_url():
+    table = view_handlers.get_and_populate_shoot_table()
 
+    for name, video in table.items():
+        stripped_name = name.replace(',','').replace(' ','')
+        vcr_path = 'test/vcr/test_IWC_integration/video{}.yaml'.format(stripped_name)
 
-# def test_image_from_video_url():
-#     table = utils.get_table()
-
-#     for name, video in table.items():
-#         stripped_name = name.replace(',','').replace(' ','')
-#         vcr_path = 'test/vcr/test_IWC_integration/video{}.yaml'.format(stripped_name)
-
-#         with vcr.use_cassette('test/vcr/test_IWC_integration/video_{}.yaml'.format(stripped_name)):
-#             image = IWC_integration.image_from_video_url(video['link'])
-#             assert image[-4:] == '.gif'
+        with vcr.use_cassette('test/vcr/test_IWC_integration/video_{}.yaml'.format(stripped_name)):
+            image = IWC_integration.image_from_video_url(video['link'])
+            assert image[-4:] == '.gif'
